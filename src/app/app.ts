8b747021dea55c129ef4df5f0adb821476e78bb6
Martin Angulo <chavezangulo9@gmail.com>
2026-06-08 16:26:37 -0600

    Cambios que no se agregaron el dia del laboratorio.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Lab8_Angular');
+export class App implements OnInit {
+  personas: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    const headers = new HttpHeaders({'X-API-KEY': 'abc123',});
+
+    this.http.get<any[]>('http://localhost:5002/api/persona', { headers })
+      .subscribe({
+        next: data => {this.personas = data;},
+        error: error => {
+          console.error(error);
+        }
+      });
+  }
 }
